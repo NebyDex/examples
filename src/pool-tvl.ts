2a0df9b38fc53c4476e36f56f0f53439be13de19
KovacZan <zan.kovi@proton.me>
2025-03-21 11:31:06 +0100

    feat: Pool tvl example

---
+++ src/pool-tvl.ts
@@ -0,0 +1,52 @@
+import gql from "graphql-tag";
+import { createClient, fetchExchange } from "urql";
+
+const NEBY_SUBGRAPH_URL =
+  "https://api.goldsky.com/api/public/project_clzi4lu67khgw01072ibvekvt/subgraphs/neby-dex-sapphire-mainnet/1.0.0/gn";
+
+const query = gql`
+  query getPoolTVL($poolAddress: ID!) {
+    pool(id: $poolAddress) {
+      id
+      token0 {
+        id
+        symbol
+        decimals
+      }
+      token1 {
+        id
+        symbol
+        decimals
+      }
+      liquidity
+      totalValueLockedUSD
+    }
+  }
+`;
+
+async function getNebyPoolTVL(poolAddress: string) {
+  const client = createClient({
+    url: NEBY_SUBGRAPH_URL,
+    exchanges: [fetchExchange],
+  });
+
+  const variables = { poolAddress: poolAddress.toLowerCase() };
+
+  const response = await client.query(query, variables).toPromise();
+
+  const tvlUSD = parseFloat(response.data.pool.totalValueLockedUSD);
+
+  console.log("Pool ID:", response.data.pool.id);
+  console.log("Token0:", response.data.pool.token0.symbol);
+  console.log("Token1:", response.data.pool.token1.symbol);
+  console.log("Liquidity:", response.data.pool.liquidity);
+  console.log("Total Value Locked (USD):", tvlUSD.toFixed(2));
+}
+
+const main = async () => {
+  const poolAddress = "0x3a2976b76b9af12639878b87b4ccef2e951ca5aa";
+
+  await getNebyPoolTVL(poolAddress);
+};
+
+void main();
